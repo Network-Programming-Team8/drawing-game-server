6013b32bd419519c54c4f79ae472e1b807167b1e
Yunjeong <yunjeongiya@gmail.com>
2024-12-01 13:28:00 +0900

    refactor: define explicit serialVersionID in Event, Message

--- src/dto/event/Event.java
+++ src/dto/event/Event.java
@@ -1,7 +1,10 @@
 package dto.event;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public abstract class Event implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 5705597506885490633L;
 }
--- src/message/Message.java
+++ src/message/Message.java
@@ -2,10 +2,13 @@ package message;
 
 import dto.event.Event;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Message implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = -8326171520520566725L;
     private final MessageType msgType;
     private final Event event;
 
