93ea5a97039e28126357cba4cda7eae6b81e437a
Yunjeong <yunjeongiya@gmail.com>
2024-12-01 16:46:00 +0900

    feat: add logs for connection

--- src/main/GameMain.java
+++ src/main/GameMain.java
@@ -6,6 +6,7 @@ import service.UserManager;
 
 public class GameMain {
     public static void main(String[] args) {
+        System.out.println("Game Main Start");
         GameRoomManager roomManager = new GameRoomManager();
         UserManager userManager = new UserManager();
         ConnectionListener cl = new ConnectionListener(roomManager, userManager);
