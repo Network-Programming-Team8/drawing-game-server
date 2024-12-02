a154fae4bb312c27d2324cb66755de6ba4a5b076
GITiramisu <hansw0210@naver.com>
2024-12-02 21:30:54 +0900

    refactor: change a ClientDrawEvent in a server event package to a ServerDrawEvent

--- src/dto/event/server/ServerDrawEvent.java
+++ src/dto/event/server/ServerDrawEvent.java
@@ -5,14 +5,14 @@ import dto.info.DrawElementInfo;
 
 import java.io.Serial;
 
-public class ClientDrawEvent extends Event {
+public class ServerDrawEvent extends Event {
 
     @Serial
     private static final long serialVersionUID = -6086850118146047662L;
     private final int drawer;
     private final DrawElementInfo drawing;
 
-    public ClientDrawEvent(int drawer, DrawElementInfo drawing) {
+    public ServerDrawEvent(int drawer, DrawElementInfo drawing) {
         this.drawer = drawer;
         this.drawing = drawing;
     }
