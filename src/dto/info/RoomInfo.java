358538cbc2729081b530c1164c41a7450b9a5090
imscow11253 <seaweed8759@naver.com>
2024-12-04 00:32:49 +0900

    refactor : RoomInfo/UserInfo serializable화

--- src/dto/info/RoomInfo.java
+++ src/dto/info/RoomInfo.java
@@ -1,8 +1,14 @@
 package dto.info;
 
+import dto.event.Event;
+
+import java.io.Serial;
 import java.util.List;
 
-public class RoomInfo {
+public class RoomInfo extends Event {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     private final int id;
     private final int drawTimeLimit;
     private final int participantLimit;
