4c20a5e2eed83ee2f12b1fe47e3bbc16e6cb5680
GITiramisu <hansw0210@naver.com>
2024-12-05 02:06:14 +0900

    feat: Vote

---
+++ src/domain/Vote.java
@@ -0,0 +1,68 @@
+package domain;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import dto.event.Event;
+import dto.event.server.ServerFinishVoteEvent;
+import dto.event.server.ServerRequestVoteEvent;
+import dto.info.VoteInfo;
+import mapper.VoteMapper;
+import message.Message;
+import message.MessageType;
+import exception.GameServerException;
+import network.Sender;
+
+public class Vote {
+
+    private final Sender sender;
+    private final Room room;
+    private final Map<Integer, Integer> voteCounter;
+    private final int voteTimeLimit;
+    private boolean isVoteEnd;
+
+    public Vote(Room room, Sender sender){
+        this.sender = sender;
+        this.room = room;
+        this.voteCounter = new HashMap<>();
+        this.voteTimeLimit = 30;
+        this.isVoteEnd = false;
+        room.vote = this;
+    }
+
+    public void startVote() throws InterruptedException, GameServerException {
+        requestVote();
+        wait(voteTimeLimit * 1000L);
+        finishVote();
+    }
+
+    private void requestVote() throws GameServerException {
+        Event event = new ServerRequestVoteEvent(voteTimeLimit);
+        Message message = new Message(MessageType.SERVER_REQUEST_VOTE_EVENT, event);
+        broadcastIn(message);
+    }
+
+    private void finishVote() throws GameServerException {
+        Event event = new ServerFinishVoteEvent(VoteMapper.toVoteInfo(room));
+        Message message = new Message(MessageType.SERVER_FINISH_VOTE_EVENT, event);
+        broadcastIn(message);
+        isVoteEnd = true;
+    }
+
+    private void broadcastIn(Message message) throws GameServerException {
+        sender.sendToAll(message, room.getUserList().stream().map(User::getId).toList());
+    }
+
+    public void vote(int id){
+        if(voteCounter.containsKey((id))){
+            voteCounter.put(id, voteCounter.get(id)+1);
+        }
+        else{
+            voteCounter.put(id, 1);
+        }
+    }
+
+    public boolean isVoteEnd() { return isVoteEnd; }
+
+    public Map<Integer, Integer> getVoteState() { return voteCounter; }
+}
