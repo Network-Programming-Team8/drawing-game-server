441db4fd89459bd463a7a4fdbb7ea23b6a5b7aa5
GITiramisu <hansw0210@naver.com>
2024-12-06 00:38:59 +0900

    feat: complete Chat

--- src/domain/Chat.java
+++ src/domain/Chat.java
@@ -1,4 +1,22 @@
 package domain;
 
+import dto.event.Event;
+import dto.event.server.ServerRoomChatMessage;
+import exception.GameServerException;
+import message.Message;
+import message.MessageType;
+
 public class Chat {
+
+    private final Room room;
+
+    public Chat(Room room){
+        this.room = room;
+    }
+
+    public void chatting(String from, String content) throws GameServerException {
+        Event event = new ServerRoomChatMessage(from, content);
+        Message message = new Message(MessageType.SERVER_ROOM_CHAT_MESSAGE, event);
+        room.broadcast(message);
+    }
 }
