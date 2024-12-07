0dd7a0683956f2992ee94f82f8e67bb85db24dc8
Yunjeong <yunjeongiya@gmail.com>
2024-12-08 00:33:43 +0900

    hotfix: problem with selectGuesser when multiple users submitted same topics

--- src/domain/GameSetter.java
+++ src/domain/GameSetter.java
@@ -41,47 +41,16 @@ public class GameSetter {
     }
 
     private Game setGame() throws GameServerException {
-        String topic = selectTopic();
         List<Integer> order = setOrder();
+        String topic = topicSuggestionMap.get(order.get(0));
+        order.subList(1, order.size());
         int guesserId = order.get(order.size() - 1);
         return new Game(room, topic, guesserId, order, room.getDrawTimeLimit());
     }
 
-    private String selectTopic() {
-        // 모든 제안된 토픽을 리스트로 변환
-        List<String> topics = new ArrayList<>(topicSuggestionMap.values());
-        // 무작위로 토픽 선택
-        Collections.shuffle(topics);
-        return topics.get(0);
-    }
-
-    private int selectGuesser() {
-        // 선택된 토픽
-        String selectedTopic = selectTopic();
-        // 선택된 토픽을 제안하지 않은 유저 ID 목록 생성
-        List<Integer> eligibleGuessers = new ArrayList<>();
-        for (Map.Entry<Integer, String> entry : topicSuggestionMap.entrySet()) {
-            if (!entry.getValue().equals(selectedTopic)) {
-                eligibleGuessers.add(entry.getKey());
-            }
-        }
-        // 무작위로 guesser 선택
-        Collections.shuffle(eligibleGuessers);
-        return eligibleGuessers.get(0);
-    }
-
     private List<Integer> setOrder() {
-        // 방에 있는 모든 유저의 ID 가져오기
         List<Integer> userIds = new ArrayList<>(room.getUserIdList());
-        // 추측자(guesser) ID 가져오기
-        int guesserId = selectGuesser();
-
-        // 추측자를 제외하고 섞기
-        userIds.remove((Integer) guesserId);
         Collections.shuffle(userIds);
-
-        // 추측자를 리스트의 마지막에 추가
-        userIds.add(guesserId);
         return userIds;
     }
 
